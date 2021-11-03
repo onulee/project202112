8411437a842366a5cdfbe0f9c57db97c4fef5322
koreavc1 <koreavc1@DESKTOP-MB8P49O>
2021-11-03 17:48:44 +0900

    11월 03일 오후 5:48 controller

--- s1102/src/main/java/com/site/ex/controller/MainController.java
+++ s1102/src/main/java/com/site/ex/controller/MainController.java
@@ -7,11 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.site.ex.beans.Member;
+
 @Controller
 public class MainController {
 	
+	
+	
 	@RequestMapping("/board/write_view")
-	public String write_view() {
+	public String write_view(Member member) {
+		
 		return "board/write_view";
 	}
 	
